5604064c351a169e291aaa10505b878fc0accd82
ChetreNelson <chetreenelson8@gmail.com>
2026-02-12 17:53:49 -0500

    little bit up the refactor on the  useonlinestatus for correct navigation of user internet status

--- firstapp/src/Hooks/custom/useStatusBar.js
+++ firstapp/src/Hooks/custom/useStatusBar.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
 export default function useOnlineStatus() {
-  const [isOnline, setIsOnline] = useState(true);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  // can also use actual api hit like on the fav icon of google so that even when wifi is on can know if internet acess is there or not
   useEffect(() => {
     function handleOnline() {
       setIsOnline(true);
@@ -11,10 +12,10 @@ export default function useOnlineStatus() {
     }
     window.addEventListener("online", handleOnline);
     window.addEventListener("offline", handleOffline);
-    return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
-    };
+    // return () => {
+    //   window.removeEventListener("online", handleOnline);
+    //   window.removeEventListener("offline", handleOffline);
+    // };
   }, []);
   return isOnline;
 }
